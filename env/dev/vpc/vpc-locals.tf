eabf04c5c379b84390ea8407b2858b080b6f3fa6
Wasim Alam <alamwasim29@gmail.com>
2022-11-12 23:46:17 +0530

    small changes in naming files

--- env/dev/vpc/vpc-locals.tf
+++ env/dev/vpc/vpc-locals.tf
@@ -1,15 +1,15 @@
 locals {
-  vpc_name             = join("-", slice(split("/", path.cwd), 5, 7))
+  vpc_name             = join("-", slice(split("/", path.cwd), 6, 8))
   vpc_cidr_block       = "10.0.0.0/16"
-  vpc_private_subnets  = [for i in range(1, 5, 2) : cidrsubnet(local.vpc_cidr_block, 8, i)]
-  vpc_public_subnets   = [for i in range(2, 5, 2) : cidrsubnet(local.vpc_cidr_block, 8, i)]
-  vpc_database_subnets = [for i in range(0, 5, 3) : cidrsubnet(local.vpc_cidr_block, 8, i)]
+  vpc_private_subnets  = [for i in range(10, 12) : cidrsubnet(local.vpc_cidr_block, 8, i)]
+  vpc_public_subnets   = [for i in range(100, 102) : cidrsubnet(local.vpc_cidr_block, 8, i)]
+  vpc_database_subnets = [for i in range(200, 202) : cidrsubnet(local.vpc_cidr_block, 8, i)]
 }
 
 locals {
   owners = var.business_division
   common_tags = {
     owners      = local.owners
-    environment = join("-", slice(split("/", path.cwd), 5, 6))
+    environment = join("-", slice(split("/", path.cwd), 6, 7))
   }
 }

e31b9822704b44226c800e08d34e8a6cd5ecf488
Wasim Alam <alamwasim29@gmail.com>
2022-12-23 16:40:22 +0530

    pvt-sg,pvt-subnet added in output and sg modified in locals

--- env/dev/vpc/vpc-out.tf
+++ env/dev/vpc/vpc-out.tf
@@ -1,10 +1,25 @@
-output "public_subnet_id" {
+output "pub_subnet_id" {
   description = "vpc public subnet id to be used in compute instances."
-  value       = module.vpc.public_subnets[0]
+  value       = module.vpc.public_subnets
+}
+output "pvt_subnet_id" {
+  description = "vpc private subnet id to be used in compute instances."
+  value       = module.vpc.private_subnets
 }
 
-output "public_security_group" {
+output "pub_security_group" {
   description = "sg for instances in public group."
   value       = module.security["public_sg"].security_group_id
+
+}
+output "pvt_security_group" {
+  description = "sg for instances in private group."
+  value       = module.security["private_sg"].security_group_id
+
+}
+
+output "vpc_id" {
+  description = "vpc id to be used by other resources."
+  value       = module.vpc.vpc_id
 
 }

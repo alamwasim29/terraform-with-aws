8a70e0881fce7e59ad64ff97d6710105f50a0481
Wasim Alam <alamwasim29@gmail.com>
2023-01-06 21:01:38 +0530

    contains function usage: creating more than 2 instances in 2 subnet with same code using count

--- env/dev/compute/compute-private-main.tf
+++ env/dev/compute/compute-private-main.tf
@@ -2,14 +2,14 @@ module "ec2_pvt" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "4.2.0"
   # insert the 10 required variables here
-  name          = "${local.owners}-${local.common_tags.environment}-ec2-pvt"
+  name          = contains([for i in range(0, var.pvt_instance_count / 2) : i], count.index) ? "${local.common_tags.environment}-${count.index}-app1" : "${local.common_tags.environment}-${count.index}-app2"
   count         = var.pvt_instance_count
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.pub_key_name
   #monitoring             = true
-  subnet_id              = data.terraform_remote_state.vpc.outputs.pvt_subnet_id[count.index]
+  subnet_id              = contains([for i in range(0, var.pvt_instance_count, 2) : i], count.index) ? data.terraform_remote_state.vpc.outputs.pvt_subnet_id[0] : data.terraform_remote_state.vpc.outputs.pvt_subnet_id[1]
   vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.pvt_security_group]
-  user_data              = file("../../script/app1.sh")
+  user_data              = contains([for i in range(0, var.pvt_instance_count / 2) : i], count.index) ? file("../../script/app1.sh") : file("../../script/app2.sh")
   tags                   = local.common_tags
 }

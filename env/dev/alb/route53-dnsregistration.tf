31512fbf56b149ce15e273f52e3f1b05697fbc99
Wasim Alam <alamwasim29@gmail.com>
2023-01-12 12:36:40 +0530

    route53 resource modified to create 3 record

--- env/dev/alb/route53-dnsregistration.tf
+++ env/dev/alb/route53-dnsregistration.tf
@@ -1,6 +1,7 @@
 resource "aws_route53_record" "app_dns" {
+  count   = 3
   zone_id = data.aws_route53_zone.mydomain.zone_id
-  name    = "apps.husna.cloud"
+  name    = "${var.headers[count.index]}.husna.cloud"
   type    = "A"
   alias {
     name                   = module.alb.lb_dns_name
@@ -9,25 +10,36 @@ resource "aws_route53_record" "app_dns" {
   }
 
 }
-resource "aws_route53_record" "app_dns" {
-  zone_id = data.aws_route53_zone.mydomain.zone_id
-  name    = "app1.husna.cloud"
-  type    = "A"
-  alias {
-    name                   = module.alb.lb_dns_name
-    zone_id                = module.alb.lb_zone_id
-    evaluate_target_health = true
-  }
+# resource "aws_route53_record" "app_dns" {
+#   zone_id = data.aws_route53_zone.mydomain.zone_id
+#   name    = "apps.husna.cloud"
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }
 
-}
-resource "aws_route53_record" "app_dns" {
-  zone_id = data.aws_route53_zone.mydomain.zone_id
-  name    = "app2.husna.cloud"
-  type    = "A"
-  alias {
-    name                   = module.alb.lb_dns_name
-    zone_id                = module.alb.lb_zone_id
-    evaluate_target_health = true
-  }
+# }
+# resource "aws_route53_record" "app1_dns" {
+#   zone_id = data.aws_route53_zone.mydomain.zone_id
+#   name    = "app1.husna.cloud"
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }
 
-}
+# }
+# resource "aws_route53_record" "app2_dns" {
+#   zone_id = data.aws_route53_zone.mydomain.zone_id
+#   name    = "app2.husna.cloud"
+#   type    = "A"
+#   alias {
+#     name                   = module.alb.lb_dns_name
+#     zone_id                = module.alb.lb_zone_id
+#     evaluate_target_health = true
+#   }
+
+# }
